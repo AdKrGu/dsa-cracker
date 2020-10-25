52a8937304a9f80a54a59aea733cbe468904d552
AdityaKG-169 <f20190338@goa.bits-pilani.ac.in>
2020-10-26 03:31:07 +0530

    Added Announcements section

--- src/Components/Announcement/Announcement.js
+++ src/Components/Announcement/Announcement.js
@@ -1,18 +1,17 @@
 import React from "react";
 import "./announcement.css";
-import { Link } from "react-router-dom";
 
 const Announcement = () => {
 	return (
 		<div>
 			{window.location.pathname === "/profile" ? (
-				<Link to="/upload" style={{ color: "white", textDecoration: "none" }}>
+				<a href="/upload" style={{ color: "white", textDecoration: "none" }}>
 					<p className="announcement">
 						Submit video solution of any{" "}
 						<span style={{ color: "yellow" }}>Golden</span> question and get
 						questions from past FAANG interviews. Click Here!
 					</p>
-				</Link>
+				</a>
 			) : null}
 		</div>
 	);
